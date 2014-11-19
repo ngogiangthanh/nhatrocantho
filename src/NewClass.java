3ec089b5dd9726bafc8909e3191a2cde36a11bbf
nhandinh1992 <nhandinh1992@a5c743af-c50f-bc39-5325-da8c9df85209>
2014-11-19 19:40:28 +0000

    lam xong mot so van de! anh chị vào xem phan thay đổi code nhe! riêng trong class KetNoi phần URL file CSDL thì mình làm là địa chỉ tuyệt đối, anh chị check out ve sủa lại giùm mình nhe! tại đường dẩn tương đối trên máy mình ko build dc!!!! thank all.

---
+++ src/NewClass.java
@@ -0,0 +1,26 @@
+
+import GUIs.f_dangnhap;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import models.TaiKhoan;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Dinh Nhan
+ */
+public class NewClass {
+  /*  public static void main(String[] args) {
+               ObjectContainer db = Db4oEmbedded.openFile("E:\\thac_si\\hk1_2014\\CSDL_HDT\\nhatrocantho\\src\\database\\containers.dbo");
+               TaiKhoan tk = new TaiKhoan("nhan", "nhan", (byte) 1.0);
+               db.store(tk);
+               db.commit();
+               db.close();
+               
+    }*/
+    
+}
